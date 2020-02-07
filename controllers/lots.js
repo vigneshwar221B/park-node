aab12c4590d4ab865cc71f2ea4f86c46200ca825
Vignesh War <vigneshwar@Vigneshs-MacBook-Pro.local>
2020-02-07 21:29:54 +0530

    jhsbdi

--- controllers/lots.js
+++ controllers/lots.js
@@ -1,18 +1,17 @@
 const Parking = require('../model/Parking')
 
+//5e3d6037c75c8e3adc4caaac
 exports.checkLots = async (req, res) => {
-	const lots = await Parking.find({})
+	const lots = await Parking.findOne({_id: '5e3d6037c75c8e3adc4caaac'})
 	res.send(lots)
 }
 
 exports.updateLots = async (req, res) => {
 	const { arr } = req.body
+	const lots = await Parking.findOne({_id: '5e3d6037c75c8e3adc4caaac'})
+	lots.arr = arr
 
-	const parking = new Parking({
-		arr,
-	})
-
-	await parking.save()
+	await lots.save()
 
 	res.send({msg: 'done'})
 }
